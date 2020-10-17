2aa246cbf1dbc2913e267e212002e0856c050a90
Lee kwang ho <lex.less38@gmail.com>
2020-10-17 21:59:04 +0900

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from model import Generator
 from model import Discriminator
 import torch
 import torch.optim as optim
-from dataset import FaceData
+from dataset import FaceData, TrainData
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
@@ -17,11 +17,11 @@ def denorm(x):
 def main():
     batch_size = 16
     generator = Generator().cuda()
-    discriminator = Discriminator().cuda()
-    optimizer_G = optim.Adam(generator.parameters()
-    )
-    optimizer_D = optim.Adam(discriminator.parameters())
-    dataset = FaceData('train')
+    discriminator = Discriminator(96,96).cuda()
+    optimizer_G = optim.Adam(generator.parameters(), lr=1e-4)
+    optimizer_D = optim.Adam(discriminator.parameters(), lr=1e-4)
+    # dataset = FaceData('train')
+    dataset = TrainData()
     data_loader = DataLoader(dataset, batch_size, shuffle=True, num_workers=0, pin_memory=True, drop_last=True)
     MSE = nn.MSELoss()
     BCE = nn.BCELoss()
@@ -44,7 +44,7 @@ def main():
 
             img_GT = img_GT.cuda()
             img_Input = img_Input.cuda()
-
+            
             # # Discriminator update
             # img_SR = generator(img_Input)
             # fake = discriminator(img_SR)
@@ -69,18 +69,29 @@ def main():
             # loss_G.backward()
             # # loss_Dfake.backward()
             # optimizer_G.step()
-
+            if epoch < 10:
+                # SRResnet Initialize Generator update
+                generator.zero_grad()
+                img_SR = generator(img_Input)
+                loss_content = MSE(img_SR, img_GT)
+                loss_content.backward()
+                optimizer_G.step()
+                
+                if step%100 == 0:
+                    print()
+                    print("Loss_content : {}".format(loss_content.item()))
+                continue
 
             # Discriminator update
             discriminator.zero_grad()
             D_real = discriminator(img_GT)
-            loss_Dreal = BCE(D_real, torch.ones(batch_size, 1).cuda())
+            loss_Dreal = 0.1 * BCE(D_real, torch.ones(batch_size, 1).cuda())
             loss_Dreal.backward()
             D_x = D_real.mean().item()
 
             img_SR = generator(img_Input)
             D_fake = discriminator(img_SR.detach())
-            loss_Dfake = BCE(D_fake, torch.zeros(batch_size, 1).cuda())
+            loss_Dfake = 0.1 * BCE(D_fake, torch.zeros(batch_size, 1).cuda())
             loss_Dfake.backward()
             DG_z = D_fake.mean().item()
 
@@ -102,7 +113,7 @@ def main():
             optimizer_G.step()
 
 
-            if step%10 == 0:
+            if step%100 == 0:
                 # :.10f
                 print()
                 print("fake out : {}".format(DG_z))
@@ -110,17 +121,19 @@ def main():
                 print("Loss_Dfake :   {}".format(loss_Dfake.item()))
                 print("Loss_Dreal :   {}".format(loss_Dreal.item()))
                 print("Loss_D :       {}".format(loss_D.item()))
-                print("Loss_vgg :     {}".format(loss_vgg.item()))
                 print("Loss_content : {}".format(loss_content.item()))
+                print("Loss_vgg :     {}".format(0.006*loss_vgg.item()))
+                print("Loss_Gfake :   {}".format(0.001*loss_Gfake.item()))
                 print("Loss_G :       {}".format(loss_G.item()))
                 print("Loss_Total :   {}".format((loss_G + loss_D).item()))
                 # print("Loss_D : {:.4f}".format(loss_D.item()))
                 # print("Loss : {:.4f}".format(loss_total.item()))
 
-        generator.eval()
-        save_image(denorm(img_SR[0].cpu()), "./Result/{0}_SR.png".format(epoch))
-        save_image(denorm(img_GT[0].cpu()), "./Result/{0}_GT.png".format(epoch))
-        generator.train()
+        with torch.no_grad():
+            generator.eval()
+            save_image(denorm(img_SR[0].cpu()), "./Result/{0}_SR.png".format(epoch))
+            save_image(denorm(img_GT[0].cpu()), "./Result/{0}_GT.png".format(epoch))
+            generator.train()
 
 if __name__ == "__main__":
     main()
